e1507cc33583cb15a2ffa053248fa0f1ff29351c
collier1 <collier1@purdue.edu>
2018-02-04 15:21:18 -0500

    Added navBar

---
+++ uCalAngular/src/app/nav-bar/nav-bar.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit, HostListener } from '@angular/core';
+
+@Component({
+  selector: 'app-nav-bar',
+  templateUrl: './nav-bar.component.html',
+  styleUrls: ['./nav-bar.component.css']
+})
+export class NavBarComponent implements OnInit {
+  ngOnInit() {
+   
+  }
+}
