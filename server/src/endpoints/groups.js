8d0fead78123d58352d5c71517f9f87c4f6de6ce
Andrew <Bostockdrew97@gmail.com>
2018-02-06 20:50:15 -0500

    Started changing the functions to routes

--- server/src/endpoints/groups.js
+++ server/src/endpoints/groups.js
@@ -2,26 +2,55 @@ var User = require('../models/user');
 var Calendar = require('../models/calendar');
 var Group = require('../models/group');
 var Evento = require('../models/event');
+const _ = require('lodash');
+const app = require('../../server');
 
-function acceptInviteToGroup(user, groupId) {
-    var user = User.User.findByCredentials(user);
-    var group = Group.Group.findOne({
-        _id: groupId,
-        invited: {$elemMatch: user._id}
+app.post('/groups/accept', (req, res) => {
+    //acceptInviteToGroup
+    var user = _.pick(req.body, [user]);
+    var group = _.pick(req.body, [groupId]);
+    User.User.findByCredentials(user).then((user) => {
+        Group.Group.findOne({
+            _id: group._id,
+            invited: {$elemMatch: user._id}
+        }).then((group) => {
+            group.invited.remove(user._id);
+            group.members.push(user._id);
+            group.save().then(() => {
+                console.log("Successfully joined group");
+                user.groupInvites.remove(group._id);
+                user.groups.push(group._id);
+                user.save().then(() => {
+                    return res.status(200).send("Successfully updated user and group");
+                }).catch(() => {
+                    return res.status(418).send("Failed to update the user");
+                });
+            }).catch(() => {
+                return res.status(400).send("Failed to update the user and group");
+            });
+        }).catch((err) => {
+            return res.status(404).send("Group not Found");
+        });
+    }).catch((err) => {
+        return res.status(401).send("Invalid Login");
     });
-    group.invited.remove(user._id);
-    group.memebers.push(user._id);
-    return group;
-}
+});
 
-function createGroup(creator, groupData) {
-    var creator = User.User.findByCredentials(creator);
-    var group = new Group({
-        name: groupData.name,
-        creator: creator._id
+app.post('/groups/create', (res, req) => {
+    //Cannot Invite people here
+    var user = _.pick(res.body, [user]);
+    var group = _.pick(res.body, [group]);
+    User.User.findByCredentials(user).then((user) => {
+        group = new Group(group);
+        group.save().then(() => {
+            return res.status(200).send("Group Created");
+        }).catch(() => {
+            return res.status(400).send("Failed to create group");
+        });
+    }).catch(() => {
+        return res.status(401).send("Invalid Login");
     });
-    return group;
-}
+})
 
 function declineInviteToGroup(user, groupId) {
     var user = User.User.findByCredentials(user);
