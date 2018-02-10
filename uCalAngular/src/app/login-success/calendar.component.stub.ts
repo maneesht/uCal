5ee0f8ec0a69290c4f7d322af78f8b61623b3f9a
Maneesh Tewani <maneesh.tewani@gmail.com>
2018-02-10 17:58:57 -0500

    Included component stub

---
+++ uCalAngular/src/app/login-success/calendar.component.stub.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'app-calendar',
+  template: `<h1> App Calendar Works! </h1>`,
+  styles: []
+})
+export class CalendarComponentStub {}
