854c1295edbfc0c68241d5239ec9ec2d3731f74d
Andrew <Bostockdrew97@gmail.com>
2018-02-10 16:26:17 -0500

    Removed some code that shouldn't have been there

--- server/src/endpoints/calendars.js
+++ server/src/endpoints/calendars.js
@@ -139,38 +139,4 @@ calendarRouter.patch('/calendars/:calendarID/share', (req, res) => {
     });
 });
 
-function createCalendar(calendarData) {
-    var calendar = new Calendar(calendarData);
-    Calendar.Calendar.findOne({name: calendar.name, owner: calendar.owner}, function(err, res) {
-        if (err || !res) 
-            continue;
-        else
-            console.log(`Calendar ${calendar.name} already exists for user ${calendar.owner}`);
-            //TODO figure out how to get out of here
-    });
-    let owner = User.User.findById(calendar.owner);
-    calendar.save(function (err) {
-        if (err)
-            console.error(err);
-        else
-            console.log(`New calendar "${calendar.name}" created for user ${calendar.owner}`);
-    });
-    owner.calendars.push(calendar._id);
-    owner.save(function (err) {
-        if (err) console.error(err);
-        else console.log("Calendar added to user");
-    });
-}
-
-function deleteCalendar(calendarId) {
-    Calendar.Calendar.findByIdAndRemove(calendarId);
-}
-
-function getCalendar(calendarId) {
-    var calendar = Calendar.Calendar.findById(calendarId).lean();
-}
-
-function updateCalendar(calendarData) {
-    //TODO
-}
 module.exports = calendarRouter;
