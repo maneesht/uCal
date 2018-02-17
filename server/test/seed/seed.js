5d1f46b63a31780d24b49f935ed36082b0419fdf
Maneesh Tewani <maneesh.tewani@gmail.com>
2018-02-17 16:56:18 -0500

    Fixed messed up spacing

--- server/test/seed/seed.js
+++ server/test/seed/seed.js
@@ -15,112 +15,112 @@ const groupOneId = new ObjectID();
 const groupTwoId = new ObjectID();
 
 const users = [{
-	_id: userOneId,
-	email: 'eli@example.com',
-	password: 'UserPassOne',
-	groups: [
-		groupOneId
-	],
-	groupinvites: [
-		groupTwoId
-	]
+    _id: userOneId,
+    email: 'eli@example.com',
+    password: 'UserPassOne',
+    groups: [
+        groupOneId
+    ],
+    groupinvites: [
+        groupTwoId
+    ]
 }, {
-	_id: userTwoId,
-	email: 'steve@example.com',
-	password: 'UserPassTwo',
-	groups: [
-		groupTwoId
-	]
+    _id: userTwoId,
+    email: 'steve@example.com',
+    password: 'UserPassTwo',
+    groups: [
+        groupTwoId
+    ]
 }];
 
 const calendars = [{
-	_id: calendarOneId,
-	name: "Test Calendar 1",
-	description: "The description!",
-	owner: userOneId,
-	users: users[0]
+    _id: calendarOneId,
+    name: "Test Calendar 1",
+    description: "The description!",
+    owner: userOneId,
+    users: users[0]
 }, {
-	_id: calendarTwoId,
-	name: "Test Calendar 2",
-	description: "The description 2!",
-	owner: userTwoId,
-	users: users[1]
+    _id: calendarTwoId,
+    name: "Test Calendar 2",
+    description: "The description 2!",
+    owner: userTwoId,
+    users: users[1]
 }];
 
 const events = [{
-	_id: eventOneId,
-	name: "Test Event 1",
-	date: { day: 1, month: 1, year: 2000 },
-	allDay: false,
-	startTime: { day: 1, month: 1, year: 2000, hour: 1, minute: 30 },
-	endTime: { day: 1, month: 1, year: 2000, hour: 2, minute: 30 },
-	location: { name: "HAAS" },
-	description: "CS meeting",
-	calendar: calendarOneId,
-	invites: []
+    _id: eventOneId,
+    name: "Test Event 1",
+    date: { day: 1, month: 1, year: 2000 },
+    allDay: false,
+    startTime: { day: 1, month: 1, year: 2000, hour: 1, minute: 30 },
+    endTime: { day: 1, month: 1, year: 2000, hour: 2, minute: 30 },
+    location: { name: "HAAS" },
+    description: "CS meeting",
+    calendar: calendarOneId,
+    invites: []
 }, {
-	_id: eventTwoId,
-	name: "Test Event 2",
-	date: { day: 2, month: 2, year: 2222 },
-	allDay: false,
-	startTime: { day: 2, month: 2, year: 2222, hour: 2, minute: 00 },
-	endTime: { day: 2, month: 2, year: 2222, hour: 2, minute: 30 },
-	location: { name: "Lawson" },
-	description: "CS meeting",
-	calendar: calendarOneId,
-	invites: []
+    _id: eventTwoId,
+    name: "Test Event 2",
+    date: { day: 2, month: 2, year: 2222 },
+    allDay: false,
+    startTime: { day: 2, month: 2, year: 2222, hour: 2, minute: 00 },
+    endTime: { day: 2, month: 2, year: 2222, hour: 2, minute: 30 },
+    location: { name: "Lawson" },
+    description: "CS meeting",
+    calendar: calendarOneId,
+    invites: []
 }];
 
 const groups = [{
-	_id: groupOneId,
-	name: "Test Group 1",
-	creator: userOneId,
-	members: [userOneId]
+    _id: groupOneId,
+    name: "Test Group 1",
+    creator: userOneId,
+    members: [userOneId]
 }, {
-	_id: groupTwoId,
-	name: "Test Group 2",
-	creator: userTwoId,
-	members: [userTwoId]
+    _id: groupTwoId,
+    name: "Test Group 2",
+    creator: userTwoId,
+    members: [userTwoId]
 }];
 
 const populateUsers = (done) => {
-	User.remove({}).then(() => {
-		var userOne = new User(users[0]).save();
-		var userTwo = new User(users[1]).save();
+    User.remove({}).then(() => {
+        var userOne = new User(users[0]).save();
+        var userTwo = new User(users[1]).save();
 
-		return Promise.all([userOne, userTwo]);
+        return Promise.all([userOne, userTwo]);
 
-	}).then(() => done());
+    }).then(() => done());
 };
 
 const populateCalendars = (done) => {
-	Calendar.remove({}).then(() => {
-		var calendarOne = new Calendar(calendars[0]).save();
-		var calendarTwo = new Calendar(calendars[1]).save();
+    Calendar.remove({}).then(() => {
+        var calendarOne = new Calendar(calendars[0]).save();
+        var calendarTwo = new Calendar(calendars[1]).save();
 
-		return Promise.all([calendarOne, calendarTwo]);
+        return Promise.all([calendarOne, calendarTwo]);
 
-	}).then(() => done());
+    }).then(() => done());
 };
 
 const populateEvents = (done) => {
-	UEvent.remove({}).then(() => {
-		var eventOne = new UEvent(events[0]).save();
-		var eventTwo = new UEvent(events[1]).save();
+    UEvent.remove({}).then(() => {
+        var eventOne = new UEvent(events[0]).save();
+        var eventTwo = new UEvent(events[1]).save();
 
-		return Promise.all([eventOne, eventTwo]);
+        return Promise.all([eventOne, eventTwo]);
 
-	}).then(() => done());
+    }).then(() => done());
 };
 
 const populateGroups = (done) => {
-	UEvent.remove({}).then(() => {
-		var groupOne = new UEvent(groups[0]).save();
-		var groupTwo = new UEvent(groups[1]).save();
+    UEvent.remove({}).then(() => {
+        var groupOne = new UEvent(groups[0]).save();
+        var groupTwo = new UEvent(groups[1]).save();
 
-		return Promise.all([groupOne, groupTwo]);
+        return Promise.all([groupOne, groupTwo]);
 
-	}).then(() => done());
+    }).then(() => done());
 };
 
 module.exports = { users, calendars, events, groups, populateUsers, populateCalendars, populateEvents, populateGroups };
--- server/test/users.test.js
+++ server/test/users.test.js
@@ -14,175 +14,175 @@ beforeEach(populateUsers);
 describe('USER TESTS', () => {
 
 describe('POST /users/login', () => {
-	it('should return user', (done) => {
-		var email = users[0].email;
-		var password = users[0].password;
+    it('should return user', (done) => {
+        var email = users[0].email;
+        var password = users[0].password;
 
-		request(app)
-			.post('/login')
-			.send({email, password})
-			.expect(200)
-			.expect((res) => {
-				expect(res.body).toExist;
-				expect(res.body.message).toEqual("Success!");
-				expect(res.body.token).toExist;
-			})
-			.end(done);
-	});
+        request(app)
+            .post('/login')
+            .send({email, password})
+            .expect(200)
+            .expect((res) => {
+                expect(res.body).toExist;
+                expect(res.body.message).toEqual("Success!");
+                expect(res.body.token).toExist;
+            })
+            .end(done);
+    });
 
-	it('should return a 404 because the user cant be found', (done) => {
-		var email = 'fake@example.com';
-		var password = 'fakePass';
+    it('should return a 404 because the user cant be found', (done) => {
+        var email = 'fake@example.com';
+        var password = 'fakePass';
 
-		request(app)
-			.post('/login')
-			.send({email, password})
-			.expect(404)
-			.expect((res) => {
-				expect(res.body).toExist;
-			})
-			.end(done);
-	});
+        request(app)
+            .post('/login')
+            .send({email, password})
+            .expect(404)
+            .expect((res) => {
+                expect(res.body).toExist;
+            })
+            .end(done);
+    });
 });
 
 describe('POST /users/create', () => {
 
-	it('should return user', (done) => {
-		var email = "newemail@example.com";
-		var password = 'password';
+    it('should return user', (done) => {
+        var email = "newemail@example.com";
+        var password = 'password';
 
-		request(app)
-			.post('/users/create')
-			.send({
-				email: email,
-				password: password
-			})
-			.expect(200)
-			.expect((res) => {
-				expect(res.body).toExist;
-				expect(res.body).toContainKeys(['email']);
-				expect(res.body.email).toEqual(email);
-			})
-			.end(done);
-	});
+        request(app)
+            .post('/users/create')
+            .send({
+                email: email,
+                password: password
+            })
+            .expect(200)
+            .expect((res) => {
+                expect(res.body).toExist;
+                expect(res.body).toContainKeys(['email']);
+                expect(res.body.email).toEqual(email);
+            })
+            .end(done);
+    });
 
-	it('should return 400 because the user already exists', (done) => {
-		request(app)
-			.post('/users/create')
-			.send({
-				email: users[0].email,
-				password: users[0].password
-			})
-			.expect(400)
-			.expect((res) => {
-				expect(res.text).toExist;
-				expect(res.text).toEqual("Account already exists for: " + users[0].email);
-			})
-			.end(done);
-	});
+    it('should return 400 because the user already exists', (done) => {
+        request(app)
+            .post('/users/create')
+            .send({
+                email: users[0].email,
+                password: users[0].password
+            })
+            .expect(400)
+            .expect((res) => {
+                expect(res.text).toExist;
+                expect(res.text).toEqual("Account already exists for: " + users[0].email);
+            })
+            .end(done);
+    });
 
-	it('should return 400 because the email is improperly formatted', (done) => {
-		request(app)
-			.post('/users/create')
-			.send({
-				email: "email",
-				password: "password"
-			})
-			.expect(400)
-			.expect((res) => {
-				expect(res.text).toExist;
-				expect(res.text).toEqual("email not a correct email format");
-			})
-			.end(done);
-	});
+    it('should return 400 because the email is improperly formatted', (done) => {
+        request(app)
+            .post('/users/create')
+            .send({
+                email: "email",
+                password: "password"
+            })
+            .expect(400)
+            .expect((res) => {
+                expect(res.text).toExist;
+                expect(res.text).toEqual("email not a correct email format");
+            })
+            .end(done);
+    });
 
-	it('should return 400 because the request does not contain all information necessary', (done) => {
-		request(app)
-			.post('/users/create')
-			.send({
-				email: "email@example.com"
-			})
-			.expect(400)
-			.expect((res) => {
-				expect(res.text).toExist;
-				expect(res.text).toEqual("Required Fields Unspecified");
-			})
-			.end(done);
-	});
+    it('should return 400 because the request does not contain all information necessary', (done) => {
+        request(app)
+            .post('/users/create')
+            .send({
+                email: "email@example.com"
+            })
+            .expect(400)
+            .expect((res) => {
+                expect(res.text).toExist;
+                expect(res.text).toEqual("Required Fields Unspecified");
+            })
+            .end(done);
+    });
 });
 
 describe('PATCH /users/:userID', () => {
-	it('should return successful', (done) => {
-		var id;
-		request(app)
-			.post('/users/find')
-			.send({email: users[0].email})
-			.expect(200)
-			.expect((res) => {
-				id = res.body._id;
-			})
-			.end(() => {
-				request(app)
-					.patch('/users/' + id)
-					.send({password: "newPass"})
-					.expect(200)
-					.expect((res) => {
-						expect(res.text).toExist;
-						expect(res.text).toEqual("Password Updated Successfully");
-					})
-					.end(() => {
-						request(app)
-							.post('/login')
-							.send({email: users[0].email, password: "newPass"})
-							.expect(200)
-							.expect((res) => {
-								expect(res.body._id).toBe(users[0]._id.toHexString());
-								expect(res.body.email).toBe(users[0].email);
-								expect(res.body.password).toBe("newPass");
-							})
-							.end(done);
-					});
-			});
-	});
+    it('should return successful', (done) => {
+        var id;
+        request(app)
+            .post('/users/find')
+            .send({email: users[0].email})
+            .expect(200)
+            .expect((res) => {
+                id = res.body._id;
+            })
+            .end(() => {
+                request(app)
+                    .patch('/users/' + id)
+                    .send({password: "newPass"})
+                    .expect(200)
+                    .expect((res) => {
+                        expect(res.text).toExist;
+                        expect(res.text).toEqual("Password Updated Successfully");
+                    })
+                    .end(() => {
+                        request(app)
+                            .post('/login')
+                            .send({email: users[0].email, password: "newPass"})
+                            .expect(200)
+                            .expect((res) => {
+                                expect(res.body._id).toBe(users[0]._id.toHexString());
+                                expect(res.body.email).toBe(users[0].email);
+                                expect(res.body.password).toBe("newPass");
+                            })
+                            .end(done);
+                    });
+            });
+    });
 });
 
 describe('GET /users/:userID', () => {
-	it('should return a users information', (done) => {
-		var id;
-		request(app)
-			.post('/users/find')
-			.send({
-				email: users[0].email
-			})
-			.expect(200)
-			.expect((res) => {
-				id = res.body._id;
-			})
-			.end(() => {
-				request(app)
-					.get('/users/' + id)
-					.expect(200)
-					.expect((res) => {
-						expect(res.body).toExist;
-						expect(res.body).toContainKeys(['email', 'userId', 'groups', 'calendars', 'friends']);
-						expect(res.body.email).toEqual(users[0].email);
-						expect(res.body.userId).toEqual(id);
-						expect(res.body.groups).toBeAn(Array);
-						expect(res.body.calendars).toBeAn(Array);
-						expect(res.body.friends).toBeAn(Array);
-					})
-					.end(done);
-			});
-	});
+    it('should return a users information', (done) => {
+        var id;
+        request(app)
+            .post('/users/find')
+            .send({
+                email: users[0].email
+            })
+            .expect(200)
+            .expect((res) => {
+                id = res.body._id;
+            })
+            .end(() => {
+                request(app)
+                    .get('/users/' + id)
+                    .expect(200)
+                    .expect((res) => {
+                        expect(res.body).toExist;
+                        expect(res.body).toContainKeys(['email', 'userId', 'groups', 'calendars', 'friends']);
+                        expect(res.body.email).toEqual(users[0].email);
+                        expect(res.body.userId).toEqual(id);
+                        expect(res.body.groups).toBeAn(Array);
+                        expect(res.body.calendars).toBeAn(Array);
+                        expect(res.body.friends).toBeAn(Array);
+                    })
+                    .end(done);
+            });
+    });
 
-	it('should return a 404 since the user does not exist', (done) => {
-		request(app)
-			.get('/users/fakeid')
-			.expect(404)
-			.expect((res) => {
-				expect(res.text).toEqual('User with ID fakeid not Found');
-			})
-			.end(done);
-	});
+    it('should return a 404 since the user does not exist', (done) => {
+        request(app)
+            .get('/users/fakeid')
+            .expect(404)
+            .expect((res) => {
+                expect(res.text).toEqual('User with ID fakeid not Found');
+            })
+            .end(done);
+    });
 });
 });
